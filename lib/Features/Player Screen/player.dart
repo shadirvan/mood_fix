45246a0e435f9913a42c5b9de9626c342c644e41
Shadirvan <Shadirvan2021@gmail.com>
2023-11-07 14:40:56 +0530

    Add Basic Functionality of Playing and Pausing

--- lib/Features/Player Screen/player.dart
+++ lib/Features/Player Screen/player.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mood_fix/Constants/colors.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class PlayerScreen extends StatefulWidget {
   const PlayerScreen({super.key});
@@ -9,8 +10,54 @@ class PlayerScreen extends StatefulWidget {
 }
 
 class _PlayerScreenState extends State<PlayerScreen> {
+
+  final player = AudioPlayer();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: appBlue,body: SafeArea(child: Center(child: Text("Fuck You"),)),);
+
+    player.setSourceUrl("https://file-examples.com/storage/fed001dfc36547d0292f8e5/2017/11/file_example_MP3_700KB.mp3");
+    
+    return Scaffold(
+      backgroundColor: appBlue,
+      body:  SafeArea(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+            // Song Title
+            const Text(
+              'Believer',
+              style: TextStyle(color: Colors.white, fontSize: 22),
+            ),
+            // Song Auther Or Singer
+            const Text(
+              'Imagine Dragons',
+              style: TextStyle(color: Colors.white, fontWeight: FontWeight.w200, fontSize: 18),
+            ),
+            ElevatedButton(onPressed: () async {
+              print(player.state);
+
+              
+
+            if(player.state == PlayerState.stopped || player.state == PlayerState.paused || player.state == PlayerState.completed){
+
+             player.resume();
+             
+             }
+              
+             if(player.state == PlayerState.playing){
+              player.pause();
+
+             
+             }
+              
+            }, child: const Text('Play'))
+
+            
+                  ],
+                ),
+          )),
+    );
   }
 }
